945add5e6cc92902a097fa3f80f2088973fb57f0
Pluto <596305774@qq.com>
2019-05-03 12:43:05 +0800

    Unit 01

--- 2020_PEEEW.c
+++ 2020_PEEEW.c
@@ -8,6 +8,108 @@
 #include <stdio.h>
 
 int main(){
-    printf("This is my challenge!");
+    int date=0;
+    for (int i = 0; i<232; i++){
+        date++;
+    }
+    printf("My English Words review challenge!");
     return 0;
 }
+
+/*
+ Unit 01
+ 
+1.State
+ statute
+ manifestation
+ statistic--statistical
+ stationary
+ statement--understatement
+ overstate
+ stateman
+ estate
+ devastate--devastating
+ workstation
+ status
+ assert
+ affirm
+ 
+2.Public
+ publication
+ republican
+ publicity--publicized--publicly
+ in public
+ mass
+ civil
+ 
+3.Law
+ claw
+ lawful
+ lawsuit
+ flaw
+ lawyer
+ legislation
+ 
+4.Mean
+ meaning-well meaning
+ meaningless--meaningfully
+ means
+ meanwhile
+ by means of--by no means
+ indicate--implication
+ 
+5.Influence
+ influential
+ impact
+ 
+6.Live
+ alive
+ deliver--delivery
+ outlive
+ liveness
+ livehood
+ live off
+ dwell--reside
+ survive--survival
+ inhabit
+ settle
+ 
+7.Federal
+ federation
+ FBI
+ CIA
+
+8.Large
+ largely
+ enlarge
+ at large
+ bulky
+ outsize
+ huge
+ 
+9.Mark
+ market--marketplace
+ marked
+ marketer
+ remark
+ remarkable
+ landmark
+ blot
+ spot
+ stain
+ symbol
+ 
+10.System
+ systematic--systematically
+ regime
+ organization
+ structure
+ 
+11.Stress
+ stressed-out
+ highlight
+ emphasize
+ strain
+ 
+ */
+
