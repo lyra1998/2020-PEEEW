76c363cdf5ab13a43265590b7e48c16af2fa5263
Pluto <596305774@qq.com>
2019-05-06 17:49:48 +0800

    Part02

---
+++ PEEEW/PEEEW/Part2.c
@@ -0,0 +1,481 @@
+//
+//  Part2.c
+//  PEEEW
+//
+//  Created by PLUTO on 2019/5/6.
+//  Copyright © 2019 PLUTO. All rights reserved.
+//
+
+void Unit06();
+/*
+ Unit06
+ 
+ 1.Identify
+ identity
+ identify with
+ recognize
+ 
+ 2.Title
+ entitle
+ entitlement
+ subtitle
+ 
+ 3.Doubt
+ doubter
+ undoubtedly
+ doubtful
+ 
+ in doubt--in no doubt
+ 
+ 4.Physical
+ physics
+ geophysical
+ physician
+ astrophysicist
+ 
+ material
+ bodily
+ 
+ 5.Introduce
+ introductory
+ 
+ launch
+ initiate
+ 
+ 6.Figure
+ figure out
+ 
+ 7.Conduct
+ semiconductor
+ condutive
+ conductor
+ 
+ behavior
+ steer
+ 
+ 8.Tell
+ teller
+ intellect--intellectual--intellectualism
+ intelligent--intelligently--intelligence
+ satellite
+ inform
+ notify
+ 
+ 9.Lack
+ blackened
+ slack
+ slcaken
+ 
+ 10.Available
+ be available to
+ obtainable
+ accessible
+ 
+ 11.Emerge
+ emergence--emergency
+ emerge from
+ surface
+ originate
+ appear
+ 
+ 12.Express
+ press
+ expression
+ impress--impressive--unimpressive
+ depress--depression
+ depressant
+ compress
+ pressing
+ oppress
+ suppress
+ press for
+ state
+ convey
+ 
+ 13.Consequence
+ in consequence
+ outcome
+ 
+ 14.Contribute
+ contributing
+ contribution
+ contribute to
+ lead to
+ give rise to
+ 
+ 15.Executive
+ 
+ 16.Current
+ curve
+ 
+ 17.Sort
+ resort
+ sort sth. out
+ assort
+ classify
+ 
+ 18.Secure
+ safeguard
+ shield
+ 
+ 19.Thread
+ menace
+ 
+ */
+
+void Unit07();
+/*
+ Unit07
+ 
+ 1.Material
+ materialism
+ materialistic
+ 
+ 2.Observe
+ observer
+ abide
+ survey
+ 
+ 3.Establish
+ establishment
+ 
+ well-established
+ establish oneself in
+ launch
+ 
+ 4.Wonder
+ wonderful
+ 
+ 5.Approach
+ access to
+ come up to
+ get close to
+ 
+ 6.Grant
+ immigrant
+ 
+ take... for granted
+ 
+ 7.Position
+ positive
+ posture
+ composition
+ opposition
+ proposition
+ disposition
+ 
+ locate
+ site
+ 
+ 8.Brain
+ brainstorm
+ brain-drain
+ 
+ 9.Term
+ determine
+ determinedly
+ terminate
+ terminal--terminally
+ in terms of
+ clause
+ 
+ 10.Increasingly
+ increased
+ 
+ 11.Labor
+ laboratory
+ elaborate
+ laborsaving
+ collaborate
+ collaboratively
+ workforce
+ 
+ 12.Mobile
+ 
+ 13.Detail
+ 
+ 14.Career
+ 
+ 15.Quit
+ resign
+ renouce
+ depart
+ 
+ 16.Essential
+ necessary
+ vital
+ indispensable
+ core
+ 
+ 17.Head
+ headache
+ go-ahead
+ headline
+ headway
+ headlong
+ headhunter
+ head for
+ chief
+ talent
+ 
+ 18.Respect
+ respected
+ respectable
+ respectively
+ irrespectively
+ with respect of
+ esteem
+ 
+ 19.Maintain
+ retain
+ uphold
+ sustain
+ 
+ 20.Primary
+ primarily
+ prime
+ principal
+ central
+ 
+ */
+
+void Unit08();
+/*
+ Unit08
+ 
+ 1.Relate
+ interrelated
+ correlate
+ correlated
+ be corrlated with
+ relate to
+ bond
+ commune
+ 
+ 2.Directly
+ directory
+ directive
+ indirectly
+ straight
+ frankly
+ 
+ 3.Otherwise
+ likewise
+ 
+ 4.Reflect
+ reflection
+ unreflecting
+ reflect on
+ 
+ 5.despite
+ in spite of
+ 
+ 6.discipline
+ 
+ 7.Land
+ mainland
+ landing
+ 
+ 8.Element
+ elevate
+ 
+ 9.Indicate
+ 
+ 10.Expose
+ pose
+ purpose
+ propose
+ impose
+ compose
+ oppose
+ suppose--supposedly
+ be exposed to
+ be composed of
+ as opposed to
+ be supposed to
+ 
+ 11.Religion
+ religious
+ 
+ 12.Suffer
+ suffering
+ suffer sth./suffer from sth
+ 
+ 13.Epidemic
+ 
+ 14.Addict
+ 
+ 15.Payment
+ 
+ 16.Hijack
+ 
+ 17.Scale
+ 
+ 18.Circumstance
+ under the circumstances = in the circumstances
+ circumstances
+ 
+ 19.Threaten
+ 
+ 20.Prospect
+ 
+ */
+
+void Unit09();
+/*
+ Unit09
+ 
+ 1.Reject
+ rejection
+ decline
+ refuse
+ discard
+ 
+ 2.Occur
+ occurence
+ incur
+ happen
+ arise
+ 
+ 3.Meet
+ assemble
+ congregate
+ 
+ 4.Personal
+ interpersonal
+ personality
+ impersonal
+ individual
+ private
+ 
+ 5.Indeed
+ certainly
+ definitely
+ 
+ 6.Manage
+ manager
+ manageable
+ management
+ administer
+ regulate
+ control
+ 
+ 7.Recognize
+ unrecognized
+ recognition
+ 
+ 8.Object
+ objection
+ objective--objectivity--objectiveness
+ object to sth./doing sth
+ 
+ 9.Trust
+ distruct
+ mistrusted
+ trust fund
+ put trust in sb./sth
+ 
+ 10.Critical
+ critically
+ hypocritical
+ criticize
+ be critical of
+ 
+ 11.Immediate
+ immediately
+ instant
+ 
+ 12.Let
+ outlet
+ let out
+ let alone
+ 
+ 13.Advocate
+ 
+ 14.Instance
+ instantly
+ instantaneously
+ for instance
+ 
+ 15.Preserve
+ maintain
+ 
+ */
+
+void Unit10();
+/*
+ Unit10
+ 
+ 1.Concept
+ conception
+ preconception
+ conceive
+ notion
+ 
+ 2.Enhance
+ enhanced
+ chancellor
+ enhancement
+ boost
+ heighten
+ 
+ 3.Reference
+ 
+ 4.Parallel
+ unparalleled
+ parachute
+ 
+ 5.Succeed
+ by succeeded by
+ 
+ 6.Conscious
+ consciousness--unconscious
+ subconscious
+ be conscious of
+ be aware of
+ deliberate
+ spontaneous
+ 
+ 7.Conservative
+ conservation
+ conserve energy
+ progressive
+ 
+ 8.Global
+ 
+ 9.Crisis
+ critic
+ criticism
+ 
+ 10.Muscle
+ muscualr
+ 
+ 11.Presence
+ present
+ represent
+ in the presence of
+ 
+ 12.Gene
+ genetic
+ genealogist
+ generate
+ 
+ 13.Code
+ encode
+ moral code
+ 
+ 14.Retail
+ retailer
+ entail
+ 
+ 15.Upset
+ upsetting
+ disturbance
+ 
+ 16.Partner
+ spouse
+ 
+ 17.Version
+ diversion
+ 
+ */
