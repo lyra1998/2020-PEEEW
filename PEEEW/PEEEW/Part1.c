9f3400eaeec7db84537f59926598b89f72788abb
Pluto <596305774@qq.com>
2019-05-06 16:54:23 +0800

    new

---
+++ PEEEW/PEEEW/Part1.c
@@ -0,0 +1,464 @@
+//
+//  2020_PEEEW.c
+//  
+//
+//  Created by PLUTO on 2019/5/3.
+//
+
+#include <stdio.h>
+
+int main(){
+    int date=0;
+    for (int i = 0; i<232; i++){
+        date++;
+    }
+    printf("My English Words review challenge!");
+    return 0;
+}
+
+void Unit01();
+/*
+ Unit 01
+ 
+1.State
+ statute
+ manifestation
+ statistic--statistical
+ stationary
+ statement--understatement
+ overstate
+ stateman
+ estate
+ devastate--devastating
+ workstation
+ status
+ assert
+ affirm
+ 
+2.Public
+ publication
+ republican
+ publicity--publicized--publicly
+ in public
+ mass
+ civil
+ 
+3.Law
+ claw
+ lawful
+ lawsuit
+ flaw
+ lawyer
+ legislation
+ 
+4.Mean
+ meaning-well meaning
+ meaningless--meaningfully
+ means
+ meanwhile
+ by means of--by no means
+ indicate--implication
+ 
+5.Influence
+ influential
+ impact
+ 
+6.Live
+ alive
+ deliver--delivery
+ outlive
+ liveness
+ livehood
+ live off
+ dwell--reside
+ survive--survival
+ inhabit
+ settle
+ 
+7.Federal
+ federation
+ FBI
+ CIA
+
+8.Large
+ largely
+ enlarge
+ at large
+ bulky
+ outsize
+ huge
+ 
+9.Mark
+ market--marketplace
+ marked
+ marketer
+ remark
+ remarkable
+ landmark
+ blot
+ spot
+ stain
+ symbol
+ 
+10.System
+ systematic--systematically
+ regime
+ organization
+ structure
+ 
+11.Stress
+ stressed-out
+ highlight
+ emphasize
+ strain
+ 
+ */
+
+void unit02();
+/*
+ Unit02
+ 
+ 1.Peer
+ gaze at
+ stare at
+ glare at
+ contemporary
+ 
+ 2.Issue
+ tissue
+ 
+ 3.Claim
+ proclaim
+ disclaim
+ acclaim
+ lay claim to
+ allege
+ contend
+ assert
+ 
+ 4.Patent
+ intellectual property
+ 
+ 5.Court
+ courteous
+ 
+ 6.Line
+ hardline
+ underline
+ decline
+ online
+ offline
+ coastline
+ deadline
+ incline
+ baseline
+ airline
+ outline
+ lineage
+ pipeline
+ guideline
+ in line with
+ series
+ row
+ boundary
+ bound
+ route
+ succession
+ procession
+ 
+ 7.Value
+ overvalue
+ devalued
+ worth
+ significance
+ 
+ 8.View
+ interview
+ viewer
+ review
+ reviewer
+ worldview
+ in view of
+ in view
+ on view
+ take sth. in view = take sth. into
+ account
+ perpective
+ 
+ 9.Individual
+ individually
+ collective
+ 
+ 10.economic
+ uneconomic
+ economics
+ economically
+ socioeconomic
+ fiscal cliff
+ IMF
+ financial
+ fiscal
+ monetary
+ 
+ 11.Create
+ creature
+ creative
+ recreate
+ creativity
+ creation
+ creationism
+ creationist
+ procreation
+ 
+ 12.Legal
+ illegal
+ lawful
+ legitimate
+ official
+ 
+ 13.Consider
+ consideration
+ reconsider
+ considerable
+ considering
+ consider ..as
+ 
+ */
+
+void Unit03();
+/*
+ Unit03
+ 
+ 1.Subject
+ subjective
+ be subject to
+ 
+ 2.Lead
+ leadership
+ mislead
+ leader
+ plead
+ side with
+ plead for
+ leading
+ lead someone on
+ lead up to
+ take the lead
+ 
+ 3.Intend
+ intend to do/ intend doing sth
+ 
+ 4.Sector
+ 
+ 5.Account
+ accountant
+ account for
+ take into account
+ leave sth. out of account
+ on account of
+ description
+ explaination
+ 
+ 6.consumer
+ consumption
+ consuming
+ consumerism
+ client
+ 
+ 7.Environment
+ environmental
+ environmentally
+ environmentalist
+ surroundings
+ atmosphere
+ situation
+ 
+ 8.Essay
+ article
+ 
+ 9.Matter
+ as a matter of fact
+ substance
+ stuff
+ material
+ 
+ 10.Fund
+ fundamental--fundamentally
+ nonfundamentalist
+ underfund
+ 
+ 11.Hold
+ household
+ holder
+ withhold
+ shareholder--shareholding
+ stockholder
+ stakeholder
+ hold back
+ hold on
+ hold out
+ hold up
+ cling
+ withstand
+ seize
+ 
+ 12.Function
+ functional
+ work
+ employ
+ 
+ 13.Evidence
+ evident
+ proof
+ confirmation
+ 
+ 14.Practice
+ in practice
+ perform
+ 
+ 15.Note
+ denote
+ noteworthy
+ take/make notes
+ 
+ */
+
+void Unit04();
+/*
+ Unit04
+ 
+ 1.Degree
+ agreement
+ Greek
+ 
+ 2.Concern
+ concerned--unconcerned
+ concern oneself with
+ as/so far as ... be cncerned
+ 
+ 3.Allow
+ shallow
+ allow for
+ 
+ 4.Product
+ production
+ productive--productivity
+ invention
+ 
+ 5.Level
+ low-level
+ level off
+ on the level
+ do one's level best
+ 
+ 6.Effort
+ effortless
+ 
+ 7.Infer
+ inferiority
+ inferior
+ inference
+ deduce
+ conclude
+ 
+ 8.Professional
+ professionalize--professionalization
+ professionalism
+ 
+ 9.Provide
+ provided
+ provider
+ provision
+ supply
+ offer
+ 
+ 10.Challenge
+ unchallenged
+
+ */
+
+void Unit05();
+/*
+ Unit05
+ 
+ 1.Deal
+ ideal
+ dealing
+ dealership
+ deal with
+ a great deal of
+ contract
+ 
+ 2.Structure
+ restructuring
+ structural
+ infrastructure
+ 
+ 3.Define
+ 
+ 4.Content
+ contention
+ discontent
+ self-contented
+ 
+ 5.Immigrant
+ immigration
+ emigrate
+ 
+ 6.Comprehension
+ comprehensive
+ beyond one's comprehension
+ 
+ 7.Potential
+ possible
+ probable
+ 
+ 8.Mass
+ massive
+ massacre
+ biomass
+ rally
+
+ 9.Compare
+ comparable
+ comparison
+ coparatively
+ compare with
+ compare to
+ contrast
+ 
+ 10.Feature
+ 
+ 11.Involve
+ involvement
+ revolve
+ evolve
+ invaild
+ engage
+ occupy
+ 
+ 12.Segment
+ section
+ fraction
+ 
+ 13.Access
+ accessible
+ access road
+ 
+ 14.Cover
+ coverage
+ recovery
+ discover
+ cloak
+ 
+ 15.Reveal
+ uncover
+ unveil
+ display
+ disclose
+ 
+ 16.Analisis
+ anatomy
+ prode
+ 
+ */
