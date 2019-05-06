56a336a3844a3e64196eb8d7849663ec57644d6d
Pluto <596305774@qq.com>
2019-05-06 16:51:28 +0800

    New

---
+++ PEEEW/PEEEW/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PEEEW
+//
+//  Created by PLUTO on 2019/5/6.
+//  Copyright © 2019 PLUTO. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
