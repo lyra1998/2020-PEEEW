ee27850867508babba1f41fadd2fc1dee986fd7e
Pluto <596305774@qq.com>
2019-05-07 13:23:06 +0800

    Part4

---
+++ PEEEW/PEEEW/Part4.c
@@ -0,0 +1,486 @@
+//
+//  Part4.c
+//  PEEEW
+//
+//  Created by PLUTO on 2019/5/7.
+//  Copyright © 2019 PLUTO. All rights reserved.
+//
+
+void Unit16();
+/*
+ Unit16
+ 
+ 1.Host
+ ghost
+ hostile
+ hostility
+ hostage
+ 
+ 2.Military
+ military intelligence
+ armed
+ 
+ 3.Native
+ alternative
+ alternatively
+ 
+ 4.Procedure
+ working procedure
+ starting procedure
+ loading procedure
+ 
+ 5.Recommend
+ recommendation
+ commend
+ 
+ 6.Master
+ masterpiece
+ mastery
+ be proficient in
+ be adept at
+ be good at
+ 
+ 7.Pour
+ outpouring
+ pour into
+ 
+ 8.Strengthen
+ strength
+ 
+ 9.Bury
+ bury one's head in the sand
+ 
+ 10.Celebrate
+ celebrity
+ 
+ 11.Crop
+ crown
+ 
+ 12.Disaster
+ catastrophe
+ 
+ 13.Orient
+ oriented
+ 
+ 14.Prevail
+ regin
+ 
+ 15.Quest
+ request
+ question
+ unquestioned
+ 
+ 16.Rear
+ rearing
+ reap
+ 
+ 17.Specialize
+ specialize in
+ 
+ 18.Vulnerable
+ fragile
+ brittle
+ 
+ 19.Ambiguous
+ unambiguous
+ obscure
+ opaque
+ vague
+ 
+ 20.Peak
+ paramount
+ summit
+ climax
+ 
+ 21.Pension
+ suspension
+ grant
+ subsidy
+ allowance
+ 
+ 22.Rage
+ outrage
+ in a rage
+ fury
+ 
+ 23.Recipient
+ 
+ 24.Eliminate
+ elimination
+ 
+ 25.Remote
+ distant
+ faraway
+ farther
+ 
+ 26.Immune
+ immune...to
+ 
+ 27.Implement
+ implementation
+ 
+ 28.Trial
+ 
+ 29.Dose
+ 
+ */
+
+void Unit17();
+/*
+ Unit17
+ 
+ 1.Expense
+ expansion
+ cost
+ outlay
+ expenditure
+ 
+ 2.Desirable
+ desire
+ advisable
+ 
+ 3.Practically
+ practicality
+ practical experience
+ realistic
+ 
+ 4.Intention
+ intentionally
+ purpose
+ intent
+ 
+ 5.Reasonable
+ reason
+ reasonably
+ reasoned
+ logical
+ 
+ 6.Democratic
+ democracy
+ democratize
+ 
+ 7.Distribute
+ distribution channel
+ 
+ 8.Promising
+ prominent
+ compromise
+ 
+ 9.Verbal
+ nonverbal
+ 
+ 10.Compete
+ competitive
+ competent
+ competently
+ incompetently
+ compete against/with
+ rival
+ 
+ 11.Distinguish
+ distinguished
+ 
+ 12.Proportion
+ 
+ 13.Appropriate
+ inappropriate
+ 
+ 14.Necessity
+ necessary
+ 
+ 15.Supply
+ supplier
+ supplement
+ 
+ 16.Emphasis
+ emphasize
+ stress
+ highlight
+ 
+ 17.Intense
+ intensive
+ 
+ */
+
+void Unit18();
+/*
+ Unit18
+ 
+ 1.Possess
+ assess
+ possession
+ assessment
+ bear
+ 
+ 2.Occupy
+ occupation
+ preoccupation
+ be occupied with
+ capture
+ 
+ 3.Odd
+ odds
+ at odds with
+ peculiar
+ 
+ 4.Strategy
+ strategic
+ tactic
+ approach
+ scheme
+ plot
+ conspiracy
+ 
+ 5.Exclusive
+ inclusive
+ 
+ 6.Confirm
+ firm
+ infirm
+ comfirm sb.in
+ affirm
+ assert
+ certify
+ testify
+ 
+ 7.Corporation
+ corporate
+ incorporate--incorporate with
+ enterprise
+ 
+ 8.Dramatic
+ dramatize
+ dramatically
+ 
+ 9.Exaggerate
+ exaggerted
+ aggravate
+ boast
+ magnify
+ 
+ 10.Endure
+ endurance
+ 
+ 11.Feeble
+ feedback
+ 
+ 12.Flourish
+ 
+ 13.Alien
+ 
+ 14.Attendance
+ attendant
+ 
+ 15.Myth
+ mystery
+ 
+ 16.Overlap
+ overseas
+ overwhelm
+ 
+ 17.Perceive
+ 
+ 18.Pilgrim
+ 
+ 19.Prestige
+ 
+ 20.Index
+ 
+ 21.Hitherto
+ 
+ 22.Impulse
+ 
+ 23.Indulge
+ 
+ 24.Inquiry
+ 
+ 25.Lean
+ 
+ 26.Resident
+ resistant--resistance
+ 
+ 27.Revolutionary
+ 
+ 28.Latter
+ late
+ later
+ isolate
+ 
+ 29.Mankind
+ humankind
+ humanity
+ creature
+ being
+ species
+ 
+ 30.Span
+ hispanic
+ spanish
+ 
+ 31.Supervise
+ supersition
+ 
+ */
+
+void Unit19();
+/*
+ Unit19
+ 
+ 1.Win
+ wine
+ windscreen
+ wind
+ swing
+ windfall
+ 
+ 2.Artificial
+ artistic
+ artificial intelligence
+ false
+ 
+ 3.Audience
+ auditor
+ 
+ 4.Bias
+ biased
+ prejudice
+ 
+ 5.Crime
+ criminal
+ discrimination
+ 
+ 6.Curiosity
+ be curious about/to
+ 
+ 7.Dispute
+ disputable
+ 
+ 8.encounter
+ counterpart
+ 
+ 9.Profound
+ profoundly
+ 
+ 10.Prosperity
+ prosperous
+ 
+ 11.Render
+ surrender
+ give up
+ yield
+ 
+ 13.Enlighten
+ unenlightened
+ acquaint
+ apprise
+ 
+ 14.Explosion
+ blast
+ bursting
+ 
+ 15.Fade
+ dissolve
+ sink
+ vanish
+ 
+ 16.Foundation
+ 
+ 17.Length
+ lengthen
+ lengthy
+ at length
+ stretch
+ 
+ 18.Panel
+ panel discussion
+ 
+ 19.Poverty
+ poor
+ poorly
+ 
+ 20.Reluctant
+ reluctantly
+ be reluctant to do sth.
+ 
+ 21.Transaction
+ 
+ 22.Accompany
+ company
+ 
+ 23.Accuracy
+ inaccuracy
+ 
+ 24.Auto
+ autonomous
+ 
+ 25.Cease
+ halt
+ suspend
+ pause
+ 
+ */
+
+void Unit20();
+/*
+ Unit20
+ 
+ 1.Contend
+ 
+ 2.Manipulate
+ manipulation
+ take charge of = be in charge of
+ 
+ 3.Council
+ the State Council
+ 
+ 4.Enthusiastic
+ enthusiasm--enthusiastically
+ 
+ 5.Inflation
+ inflationary
+ inflate
+ deflation
+ 
+ 6.Likewise
+ moreover
+ furthermore
+ 
+ 7.Outcome
+ income
+ 
+ 9.Speculate
+ 
+ 10.diffuse
+ 
+ 11.Explicit
+ explicitly
+ definite
+ 
+ 12.Fragment
+ fragmented
+ a fraction of
+ 
+ 13.Hook
+ 
+ 14.Horizon
+ 
+ 15.Lobby
+ lobbying
+ 
+ 16.Merit
+ demerit
+ 
+ 17.Penalty
+ punishment
+ 
+ 18.Sweep
+ 
+ 19.Trivial
+ triviality
+ 
+ 20.Item
+ be an item
+ 
+ 21.Disgrace
+ 
+ */
