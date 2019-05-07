5f30379703942f215b6c8a4405d5140062c74fc8
Pluto <596305774@qq.com>
2019-05-07 10:31:42 +0800

    part3

---
+++ PEEEW/PEEEW/Part3.c
@@ -0,0 +1,615 @@
+//
+//  Part3.c
+//  PEEEW
+//
+//  Created by PLUTO on 2019/5/7.
+//  Copyright © 2019 PLUTO. All rights reserved.
+//
+
+void Unit11();
+/*
+ Unit11
+ 
+ 1.Govern
+ government
+ governance
+ government-funded
+ self-governing
+ be governed by
+ administrate
+ conduct
+ 
+ 2.Interpret
+ interpretation
+ misinterpret
+ define
+ specify
+ 
+ 3.Prove
+ improve
+ proverb
+ show
+ confirm
+ 
+ 4.Return
+ returning
+ overturn
+ in return for
+ 
+ 5.Industrial
+ industry
+ industrialized
+ industrialization
+ 
+ 6.Measure
+ 
+ 7.Oppose
+ opponent
+ be opposed to
+ repel
+ deny
+ counter
+ 
+ 8.Imagine
+ image
+ imaging
+ imaginary
+ unimaginable
+ imaginative
+ dream
+ picture
+ 
+ 9.Drop
+ drop by
+ drop put
+ 
+ 10.Engage
+ engagement
+ engage in
+ engage with
+ 
+ 11.Property
+ asset
+ 
+ 12.Unemployment (rate)
+ employ
+ employer
+ employee
+ employment
+ 
+ 13.Demonstrate
+ 
+ 14.Respond
+ responsible
+ responsibility
+ corresponding
+ correspondent
+ in respond to
+ in response to
+ 
+ 15.Afford
+ unaffordable
+ afford to
+ 
+ 16.Complex
+ complexity
+ perplexing
+ intricate
+ sophisticated
+ 
+ 17.Ensure
+ assure
+ 
+ 18.Favor
+ favorite
+ favorable
+ unfavorable
+ in favor of
+ 
+ 19.Novel
+ novelty
+ 
+ 20.Requirement
+ require
+ 
+ */
+
+void Unit12();
+/*
+ Unit12
+ 
+ 1.Dominate
+ dominant
+ domestic
+ predominance
+ predominantly
+ 
+ 2.Recruit
+ 
+ 3.Priority
+ 
+ 4.Hence
+ 
+ 5.Neglect
+ negative
+ nagotiate
+ be naglectful of
+ 
+ 6.Owe
+ owing to
+ 
+ 7.Revenue
+ taxation
+ 
+ 8.Vote
+ devoted
+ election campaign
+ ballot
+ 
+ 9.Misery
+ be sorrowful of
+ grief
+ 
+ 10.Routine
+ routinely
+ circular bus route
+ 
+ 11.Inevitable
+ unavoidable
+ 
+ 12.Solve
+ resolve
+ solution
+ 
+ 13.Commerce
+ commercialized
+ commercial
+ e-commerce
+ 
+ 14.Circuit
+ 
+ 15.Integrate
+ integration
+ integrated
+ disintegrate
+ integrate with
+ 
+ 16.Disorder
+ distort
+ 
+ */
+
+void Unit13();
+/*
+ Unit13
+ 
+ 1.Highly
+ extremely
+ exceedingly
+ 
+ 2.Notice
+ at/on short notice
+ take notice of
+ advertisement
+ detect
+ 
+ 3.Obtain
+ obtainable
+ gain
+ derive
+ 
+ 4.Characterize
+ be characterized by
+ portary
+ depict
+ 
+ 5.Decline
+ on the decline
+ descend
+ decrease
+ 
+ 6.Former
+ form
+ foemal
+ formula
+ 
+ perform--performer--performance--performative
+ formation
+ reformatory
+ informed--informative--informally
+ conform--conformity
+ nonconformist
+ transformation
+ formulate--formulation
+ informativeness
+ uninformed
+ misinformation
+ uniformity
+ formidable
+ previous
+ 
+ 7.Vast
+ vastly
+ devastating
+ 
+ 8.Effective
+ effect
+ effectiveness
+ effectively
+ ineffectual
+ ineffective
+ efficient
+ 
+ 9.Justify
+ testify
+ verify
+ confirm
+ affirm
+ 
+ 10.Lie
+ underlie
+ underlying
+ lie in
+ lie with sb
+ 
+ 11.Puzzle
+ puzzlement
+ puzzled
+ confuse
+ 
+ 12.Satisfy
+ satisfied
+ dissatisfied
+ dissatisfaction
+ be satisfied with
+ be dissatisfied with
+ fulfill
+ 
+ 13.Stock
+ stock price
+ stock market
+ laughing stock
+ 
+ 14.Debate
+ controversy
+ dispute
+ 
+ 15.Equal
+ equally
+ equate
+ equality
+ equivalent
+ 
+ 16.Frequently
+ frequent
+ repeatedly
+ 
+ 17.Universal
+ universe
+ universally
+ university
+ universalize
+ overall
+ 
+ 18.Deprive
+ 
+ 19.Emotion
+ emotional
+ sentiment
+ temper
+ mood
+ disposition
+ 
+ 20.Modify
+ mode
+ modification
+ revise
+ amend
+ recast
+ 
+ 21.Net
+ network
+ intranet
+ internet-based
+ 
+ 22.Philosopher
+ philosophy
+ philophical
+ 
+ 23.Poetry
+ poet
+ 
+ 24.Remains
+ remain
+ remaining
+ 
+ 25.Balance
+ well-balanced
+ counterbalance
+ level off
+ break even
+ steady
+ stabilze
+ 
+ */
+
+void Unit14();
+/*
+ Unit14
+ 
+ 1.Elite
+ elitism
+ 
+ 2.Gradual
+ 
+ 3.Entrepreneur
+ 
+ 4.Colony
+ colonial
+ 
+ 5.Comply
+ complicate
+ complicated
+ 
+ 6.Contact
+ contest
+ in contact with
+ 
+ 7.Historian
+ 
+ 8.Inherit
+ inherent
+ 
+ 9.Privilege
+ privileged
+ 
+ 10.Purhase
+ random
+ 
+ 11.Description
+ describe
+ definition
+ picture
+ 
+ 12.Admit
+ admission
+ 
+ 13.Adopt
+ adoptive
+ 
+ 14.Crucial
+ essential
+ key
+ significant
+ desicive
+ overriding
+ 
+ 15.Quote
+ quotation
+ cite
+ 
+ 16.Restore
+ restoration
+ revise
+ recover
+ resume
+ 
+ 17.Enable
+ empower
+ permit
+ 
+ 18.Endeavor
+ strive for
+ essay
+ 
+ 19.External
+ internal
+ 
+ 20.Agenda
+ flexible schedule
+ schedule
+ 
+ 21.Cell
+ 
+ 22.Clue
+ 
+ 23.Context
+ 
+ 24.Exert
+ impose sth. on
+ 
+ 25.Option
+ optional
+ opportunity
+ exception
+ 
+ 26.Permanent
+ durable
+ 
+ 27.Trait
+ attribute
+ feature
+ peculiarity
+ 
+ 28.Cater
+ cater for
+ cater to
+ 
+ 29.Capable
+ 
+ */
+
+void Unit15();
+/*
+ Unit15
+ 
+ 1.Dependent
+ independent
+ reliant
+ 
+ 2.Rxtreme
+ extremely
+ ultimate
+ utmost
+ radical
+ 
+ 3.Imply
+ simply
+ indicate
+ infer
+ 
+ 4.Exhibit
+ exhibition
+ expose to
+ array
+ 
+ 5.Eventually
+ prevent
+ finally
+ ultimately
+ 
+ 6.Label
+ tag
+ brand
+ designate
+ 
+ 7.Psychology
+ 
+ 8.Typical
+ typically
+ sample
+ 
+ 9.Various
+ vary
+ varied
+ variety
+ varying
+ variable
+ variation
+ 
+ 10.Familiar
+ unfamiliar
+ be familiar with
+ be proficient in
+ be adept at
+ intimate
+ 
+ 11.Shift
+ downshift
+ transfer
+ alter
+ transform
+ 
+ 12.Nevertheless
+ though
+ 
+ 13.Radical
+ radically
+ 
+ 14.Sufficient
+ adequate
+ affluent
+ abundant
+ ample
+ 
+ 15.Extent
+ extension
+ extend
+ extended
+ extensive
+ to some extent
+ 
+ 16.Guarantee
+ guaranteed
+ pledge
+ 
+ 17.Convention
+ conventional
+ unconventional
+ 
+ 18.Expectation
+ expect
+ unexpected
+ expextancy
+ meet the expectation
+ 
+ 19.Dim
+ 
+ 20.Acceptance
+ 
+ 21.Clothe
+ clothes
+ 
+ 22.Lure
+ 
+ 23.Marine
+ 
+ 24.Norm
+ 
+ 25.Numerous
+ numerical
+ 
+ 26.Orchestra
+ 
+ 27.Privacy
+ 
+ 28.Illusion
+ illustrate
+ 
+ 29.Pursue
+ pursuit
+ 
+ 30.Insist
+ insistence
+ consist in/of
+ persist
+ persevere
+ uphold
+ 
+ 31.Previous
+ previously
+ primary
+ primitive
+ prior
+ 
+ 32.Release
+ publish
+ deliver
+ announce
+ 
+ 33.Remind
+ reminder
+ remind sb.of..
+ 
+ 34.Temporary
+ chronic
+ 
+ 35.Whereas
+ elsewhere
+ 
+ 36.Wise
+ otherwise
+ likewise
+ wisely
+ prudent
+ 
+ 37.Acknowledge
+ knowledge
+ knowledgeable
+ 
+ 38.Announce
+ announcement
+ declare
+ claim
+ proclaim
+ 
+ 39.Combine
+ combat
+ combine with
+ integrate
+ merge
+ 
+ 40.Enormous
+ 
+ */
+
