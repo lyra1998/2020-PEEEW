b2ea7c0dfcac1a8363101066400d21c233e63fae
Pluto <596305774@qq.com>
2019-05-08 14:58:52 +0800

    All

---
+++ PEEEW/PEEEW/Part5.c
@@ -0,0 +1,442 @@
+//
+//  Part5.c
+//  PEEEW
+//
+//  Created by PLUTO on 2019/5/8.
+//  Copyright © 2019 PLUTO. All rights reserved.
+//
+
+void Unit21();
+/*
+ Unit21
+ 
+ 1.Subtle
+ subsequent
+ 
+ 2.Bound
+ rebound
+ boundary
+ abound
+ be bound to
+ be bound up with
+ 
+ 3.Bother
+ bother to do
+ 
+ 4.Pessimistic
+ 
+ 5.Prey
+ 
+ 6.Rent
+ coherent
+ 
+ 7.Organize
+ disorganized
+ mobilize
+ arrange
+ 
+ 8.Perpetual
+ 
+ 9.Harm
+ harmful
+ harmonious
+ harsh
+ pharmaceutical
+ hurt
+ damage
+ 
+ 10.Electronic
+ electronics
+ electronically
+ 
+ 11.Rare
+ rarely
+ uncommon
+ expectional
+ 
+ 12.Treat
+ retreat
+ undertreatment
+ deal
+ handle
+ 
+ 13.Vanish
+ diminish
+ disappear
+ 
+ 14.Divide
+ dividend
+ divided
+ separate
+ split
+ 
+ 15.Mere
+ merely
+ sheer
+ simple
+ 
+ 16.Invest
+ investor
+ investment
+ investigation
+ 
+ 17.Manufacture
+ manufacturing
+ manufacturer
+ 
+ 18.Prompt
+ provoke
+ prompting
+ spur
+ 
+ 19.Table
+ confortable
+ charitable
+ acceptable
+ disputable
+ profitable
+ stable
+ unacceptable
+ suitable
+ regrettable
+ respectable
+ 
+ 20.Weigh
+ weight
+ outweigh
+ 
+ 21.Boom
+ baby boom
+ 
+ 22.Entitle
+ title
+ entitlement
+ subtitle
+ be entitled to
+ nominate
+ denominate
+ 
+ 23.Institute
+ institution
+ institutional
+ 
+ 24.Restrict
+ restriction
+ restrain sb. from doing sth
+ refrain from doing sth
+ constrain
+ curb
+ prohibit
+ forbid
+ 
+ 25.Partial
+ 
+ 26.Conclusion
+ conclusive
+ inconclusive
+ conclude
+ concluding
+ exclude
+ in conclusion
+ 
+ 27.Decorate
+ ornament
+ 
+ 28.Discard
+ get rid of
+ cast off
+ desert
+ abandon
+ 
+ */
+
+void Unit22();
+/*
+ Unit22
+ 
+ 1.Discourage
+ courage
+ courageous
+ encourage
+ 
+ 2.Devil
+ 
+ 3.Elderly
+ 
+ 4.Bulk
+ bulb
+ 
+ 5.Bypass
+ 
+ 6.Calorie
+ 
+ 7.Facilitate
+ 
+ 8.Faint
+ 
+ 9.Fashionable
+ 
+ 10.Gratitude
+ 
+ 11.Engineering
+ 
+ 12.Alliance
+ 
+ 13.Backward
+ 
+ 14.Bait
+ 
+ 15.Banquet
+ 
+ 16.Constitute
+ 
+ 17.Copyright
+ 
+ 18.Costly
+ 
+ 19.Damn
+ 
+ 20.Deputy
+ 
+ */
+
+void Unit23();
+/*
+ Unit23
+ 
+ 1.Bin
+ bind
+ 
+ 2.Bruise
+ 
+ 3.Census
+ 
+ 4.Clarity
+ charter
+ 
+ 5.Moan
+ 
+ 6.Mock
+ 
+ 7.Obsession
+ 
+ 8.Passion
+ 
+ 9.Prevalent
+ 
+ 10.Hedge
+ 
+ 11.Hypothesis
+ 
+ 12.Infant
+ 
+ 13.Intrude
+ 
+ 14.Knit
+ kit
+ 
+ 15.Repertoire
+ 
+ 16.Senate
+ 
+ 17.Handle
+ get rid of
+ dispose of
+ cope with
+ manage
+ address
+ 
+ 18.Monopoly
+ possession
+ control
+ 
+ 19.Strive
+ stripe
+ strive for
+ 
+ */
+
+void Unit24();
+/*
+ Unit24
+ 
+ 1.Adapt
+ adaptiveness
+ readmit
+ 
+ 2.Contempt
+ contemptible
+ 
+ 3.Dense
+ densely
+ 
+ 4.Exhaust
+ 
+ 5.Forecast
+ 
+ 6.Mutual
+ mutually
+ mutual benefit
+ 
+ 7.Nowhere
+ 
+ 8.Preferable
+ preference
+ 
+ 9.Rainforce
+ enforce
+ 
+ 10.Renew
+ 
+ 11.Sympathy
+ show sympathy for
+ in sympathy with
+ 
+ 12.Excel
+ excess
+ excessive
+ excellent
+ excellence
+ excel in/at
+ 
+ 13.Forth
+ set forth
+ back-and-forth
+ and so forth
+ 
+ 14.Frightening
+ 
+ 15.Frustrate
+ be frustrated by/with
+ 
+ 16.Grain
+ 
+ 17.Inferior
+ inferiority
+ 
+ 18.Initiative
+ initiate
+ initial
+ 
+ 19.Instrument
+ instrumentalist
+ 
+ 20.Mathematical
+ mathematics
+ 
+ 21.Mission
+ miss
+ commission
+ emissions-free
+ admission
+ submission
+ commissioner
+ 
+ */
+
+void Unit25();
+/*
+ Unit25
+ 
+ 1.Poisonous
+ poisoning
+ toxic
+ 
+ 2.Primitive
+ archaic
+ original
+ humble
+ 
+ 3.Printing
+ blueprint
+ print
+ printout
+ printer
+ in print
+ 
+ 4.Rush
+ gold rush
+ rush through
+ hustle
+ 
+ 5.Switch
+ switch on
+ switch off
+ 
+ 6.Temper
+ temperament
+ 
+ 7.Tone
+ milestone
+ flagstone
+ color
+ 
+ 8.Ultimate
+ ultimately
+ eventually
+ terminal
+ 
+ 9.Violent
+ terbulent
+ fierce
+ 
+ 10.Wing
+ party
+ side
+ 
+ 11.Worthy
+ noteworthy
+ worth doing sth
+ deserve sth
+ worthy of
+ 
+ 12.Alcohol
+ 
+ 13.Analogy
+ analogous
+ resemblance
+ similarity
+ 
+ 14.Anticipate
+ anticipation
+ anticipation sb.doing sth
+ 
+ 15.Column
+ 
+ 16.Commodity
+ goods
+ merchandise
+ 
+ 17.Confer
+ conference
+ confer sth.on sb
+ confer with
+ 
+ 18.Counsel
+ counseling
+ attorney
+ 
+ 19.Cultivate
+ cultivation
+ foster
+ grow
+ activate
+ 
+ 20.Deem
+ 
+ 21.Dentist
+ 
+ 22.Deteriorate
+ undermine
+ 
+ 23.Embrace
+ hug
+ 
+ 24.Evaluate
+ evaluation
+ estimate
+ 
+ 25.Glimpse
+ catch/get a glimpse of
+ 
+ */
---
+++ PEEEW/PEEEW/Part6.c
@@ -0,0 +1,758 @@
+//
+//  Part6.c
+//  PEEEW
+//
+//  Created by PLUTO on 2019/5/8.
+//  Copyright © 2019 PLUTO. All rights reserved.
+//
+
+void Unit26();
+/*
+ Unit26
+ 
+ 1.Plausible
+ 
+ 2.Literally
+ 
+ 3.Observation
+ 
+ 4.Outlook
+ prospect
+ 
+ 5.Physician
+ surspect
+ 
+ 6.Retreat
+ retreat from
+ draw back
+ yield to
+ withdraw
+ 
+ 7.Slip
+ pink slip
+ glide
+ evade
+ 
+ 8.Stable
+ 
+ 9.Substitute
+ substitute.. for
+ 
+ 10.Tide
+ 
+ 11.Via
+ 
+ 12.Adolescent
+ 
+ 13.Anonymous
+ 
+ 14.Authentic
+ 
+ 15.Chop
+ disposable chopsticks
+ bang
+ bash
+ 
+ 16.Compel
+ compelling
+ oblige
+ expel
+ impel
+ be compelled to
+ 
+ 17.Concession
+ concede sth.to sb
+ make a concession/some concessions to
+ 
+ 18.Crack
+ cracked
+ get cracking
+ gape
+ collapse
+ 
+ 19.Float
+ 
+ 20.Fond
+ fondness
+ be fond of
+ 
+ 21.Ideology
+ doctrine
+ philosophy
+ 
+ 22.Imitation
+ imitate
+ simulation
+ 
+ 23.Miserable
+ deplorable
+ 
+ 24.Parcel
+ part and parcel
+ 
+ 25.Perplex
+ perplexed
+ bewilder
+ puzzle
+ confuse
+ 
+ 26.Preach
+ interprovincial
+ adminish
+ 
+ 27.Symbol
+ sign
+ 
+ 28.Synthetic
+ 
+ 29.articulately
+ 
+ 30.Aspire
+ aspire to
+ 
+ 31.Betray
+ 
+ 32.Cement
+ displacement
+ replacement
+ pronoucement
+ 
+ 33.Comfort
+ discomfort
+ 
+ 34.Scatter
+ scattered
+ disperse
+ distribute
+ 
+ 35.Deduce
+ deductible
+ 
+ 36.Dumb
+ 
+ 37.Eligible
+ 
+ 38.Fossil
+ 
+ */
+
+void Unit27();
+/*
+ Unit27
+ 
+ 1.Logical
+ psychological
+ technological
+ geological
+ biological
+ pathological
+ astrological
+ theological
+ ecologically
+ chronological
+ 
+ 2.Definite
+ definitely
+ definition
+ indefinite
+ indefinitely
+ distinct
+ specific
+ 
+ 3.Qualify
+ be qualified for
+ modify
+ 
+ 4.Bunch
+ a bunch of
+ 
+ 5.Prolong
+ 
+ 6.Virtue
+ virtual
+ virtually
+ virtuosity
+ morality
+ merit
+ 
+ 7.Participate
+ participation
+ participatory
+ participate in
+ 
+ 8.Reduction
+ reduce
+ 
+ 9.Defend
+ defender
+ defendant
+ 
+ 10.Precise
+ precisely
+ precision
+ accurate
+ 
+ 11.Remark
+ remarkable
+ 
+ 12.Parliament
+ parliamentary
+ the House of Lords(of Britain)/the Upper House
+ the Lower House/the Lower Chamber
+ congress
+ 
+ 13.Committee
+ commit
+ commitment
+ community
+ commit oneself to
+ be committed to
+ 
+ 14.Faulty
+ fault
+ at fault
+ flaw
+ lapse
+ defect
+ drawback
+ shortcoming
+ 
+ 15.Craftsman
+ 
+ 16.Credential
+ 
+ 17.Rational
+ operational
+ 
+ 18.Licence
+ sanction
+ consent
+ certify
+ approve
+ 
+ 19.Occasionally
+ casually
+ accidentally
+ infrequently
+ randomly
+ 
+ 20.Substance
+ substantial
+ substantially
+ substantiate
+ essence
+ 
+ 21.Accelerate
+ accleraing
+ accelerated
+ escalate
+ 
+ 22.Distract
+ undistracted
+ extract
+ extravagant
+ 
+ 23.Entertainment
+ entertainer
+ entertaining
+ 
+ 24.Fatal
+ be doomed to = be bound to = be destined to
+ predict
+ prophet
+ 
+ 25.Cast
+ forecaster
+ foremost
+ 
+ 26.Glow
+ glowing
+ radiate
+ beam
+ glitter
+ 
+ 27.Incident
+ incidence
+ incidental
+ episode
+ 
+ 28.Plain
+ plateau
+ ordinary
+ 
+ 29.Quantity
+ quantitative
+ 
+ 
+ */
+
+void Unit28();
+/*
+ Unit28
+ 
+ 1.Readily
+ reader
+ 
+ 2.Stimulate
+ stimulation
+ arouse
+ motivate
+ inspire
+ 
+ 3.Textile
+ textile industry
+ 
+ 4.Geology
+ geologist
+ geological
+ ingrain
+ grace
+ 
+ 5.Gravity
+ antigravity
+ 
+ 6.Hardship
+ plight
+ predicament
+ 
+ 7.Infect
+ infection
+ 
+ 8.irrigation
+ 
+ 9.Jam
+ block
+ hinder
+ hamper
+ hurdle
+ obstacle
+ traffic jam
+ 
+ 10.Lever
+ layer
+ eve
+ 
+ 11.Metropolitan
+ 
+ 12.Mild
+ mildly
+ gentle
+ moderate
+ 
+ 13.Organic
+ organize
+ disorganized
+ organism
+ organization
+ organizational
+ 
+ 14.Phase
+ 
+ 15.Pole
+ the South Pole
+ 
+ 16.Prejudice
+ prejudice against
+ 
+ 17.Sensation
+ sensational
+ senisble
+ 
+ 18.Undertake
+ 
+ 19.Suspicious
+ suspicion
+ suspiciously
+ susceptible
+ skeptical
+ questionable
+ 
+ 20.Triumph
+ fiasco
+ 
+ 21.Vain
+ 
+ 22.Abuse
+ substance abuse
+ abuse one's authority
+ 
+ 23.Acquaint
+ acquaintance
+ be acquaintd with
+ 
+ 24.Agent
+ agency
+ 
+ 25.Burst
+ burst into
+ break into
+ outbreak
+ 
+ 26.Compensation
+ compensate for
+ 
+ 27.Coordinate
+ cooperate
+ coordinated
+ 
+ 28.Discern
+ distinguish
+ 
+ 29.Dull
+ dullness
+ tedious
+ blunt
+ 
+ */
+
+void Unit29();
+/*
+ Unit29
+ 
+ 1.Eyebrow
+ 
+ 2.Harbor
+ pearl harbor
+ shelter
+ shield
+ 
+ 3.Molecule
+ 
+ 4.Offspring
+ posterity
+ heir
+ descendant
+ 
+ 5.Orbit
+ orbital
+ 
+ 6.Portion
+ ratio
+ protein
+ proportion
+ 
+ 7.Resent
+ resentment
+ repent of
+ 
+ 8.Sake
+ forsake
+ for the sake of
+ 
+ 9.Shrink
+ forsake
+ for the sake of
+ 
+ 10.Shrink
+ shrinkage
+ 
+ 11.Sketch
+ 
+ 12.Petrol
+ patrol
+ 
+ 13.Superficial
+ shallow
+ 
+ 14.Therapy
+ therapist
+ music therapy
+ 
+ 15.Affiliate
+ affiliation
+ be affiliated/attached to
+ 
+ 16.Agitate
+ agitated
+ irritate
+ annoy
+ 
+ 17.Alphabet
+ alphabetically
+ vocabulary
+ 
+ 19.Amend
+ amendment
+ 
+ 20.Bold
+ 
+ 21.Approximate
+ approximately
+ more or less
+ approach
+ 
+ 22.Arbitrary
+ 
+ 23.Bureaucracy
+ bureaucrat
+ 
+ 24.Brake
+ 
+ 25.Circulate
+ circulation
+ 
+ 26.Catholic
+ 
+ 27.Dreadful
+ fearful
+ nasty
+ appalling
+ scary
+ 
+ 28.Contradictory
+ self-contradictory
+ paradoxical
+ 
+ 29.Digest
+ digestive
+ 
+ 30.Drought
+ famine
+ 
+ 31.Earnest
+ earnestness
+ in earnest
+ scrupulous
+ diligent
+ 
+ 32.Flavor
+ aroma
+ atmosphere
+ 
+ 33.Frontier
+ confront
+ 
+ 34.Fume
+ inflame
+ 
+ 35.Hospitality
+ be hostile to
+ hostility
+ 
+ 36.Hemisphere
+ 
+ 37.Incompetent
+ competently--incompetently
+ competent
+ incompetent to do sth.
+ 
+ 38.Hydrogen
+ 
+ 39.Ingredient
+ factor
+ element
+ compoent
+ 
+ 40.Inject
+ injection
+ inject into
+ inset
+ install
+ 
+ 41.Lest
+ 
+ 42.Junction
+ disjunction
+ conjunction
+ connection
+ joint
+ 
+ 43.Lung
+ at the top of(one's)lungs
+ 
+ 44.Microscope
+ 
+ 45.Mold
+ remold
+ mold sb/sth. into sb/sth
+ 
+ 46.Nerve
+ nervous
+ 
+ 47.Nonsense
+ 
+ */
+
+void Unit30();
+/*
+ Unit30
+ 
+ 1.Pioneer
+ forerunner
+ predecessor
+ 
+ 2.Practitioner
+ 
+ 3.Refreshment
+ refreshing
+ 
+ 4.Respectively
+ individually
+ 
+ 5.Slogan
+ motto
+ proverb
+ 
+ 6.Stumble
+ stumble across
+ stumble upon
+ tumble
+ 
+ 7.Symposium
+ 
+ 8.Vigorous
+ vigorously
+ robust
+ 
+ 9.Virus
+ 
+ 10.Abreast
+ breadth
+ abreast of
+ keep abreast of
+ 
+ 11.Weary
+ exhausted
+ 
+ 12.Divorce
+ 
+ 13.Casualty
+ incapable
+ capacity
+ 
+ 14.Cape
+ escape
+ scapegoat
+ 
+ 15.Assault
+ assault on sb.
+ 
+ 16.Bearing
+ have some bearing on
+ 
+ 17.Transfer
+ transferable
+ 
+ 18.Comedy
+ tragedy
+ 
+ 19.Zigzag
+ 
+ 20.Relieve
+ relief
+ alleviate
+ palliate
+ 
+ 21.Humble
+ 
+ 22.Stereotype
+ 
+ 23.Bible
+ 
+ 24.Impinge
+ 
+ 25.Arbiter
+ 
+ 26.Abornment
+ 
+ 27.Dweller
+ 
+ 28.Endorse
+ 
+ 29.Intrusion
+ 
+ 30.Halo
+ 
+ 31.Differentiate
+ 
+ 32.Lenient
+ leniency
+ rule out
+ 
+ 33.Nostalgia
+ 
+ 34.Rustle
+ 
+ 35.Dwarf
+ 
+ 36.Blunder
+ 
+ 37.Rut
+ 
+ 38.Anthropologist
+ 
+ 39.Mundane
+ 
+ 40.Infuriating
+ not to mention doing sth.
+ 
+ 41.Dormant
+ 
+ 42.Ecosystem
+ 
+ 43.Renaissance
+ 
+ 44.Ancestral
+ 
+ 45.Bribery
+ 
+ 46.Plump
+ 
+ 47.Panorama
+ 
+ 48.Errand
+ 
+ 49.Steward
+ 
+ 50.Slum
+ 
+ 51.Tricky
+ 
+ 52.Misinterpretation
+ 
+ 53.Demolition
+ 
+ 54.Alarmist
+ 
+ 55.Upheaval
+ 
+ 56.Aggregation
+ 
+ 57.Maladapted
+ 
+ 58.Feudalism
+ 
+ 59.Credible
+ 
+ 60.Toddler
+ 
+ 61.Medieval
+ 
+ 62.Farcical
+ 
+ 63.Personnel
+ 
+ 64.Complement
+ 
+ 65.Aversion
+ 
+ 66.Defer
+ 
+ 67.Transient
+ 
+ 68.Metric
+ 
+ 69.Compound
+ 
+ 70.Assumption
+ 
+ 71.Fluctuation
+ 
+ 72.Deficiency
+ 
+ 73.Obstruct
+ 
+ 74.Incentive
+ 
+ 
+ */
